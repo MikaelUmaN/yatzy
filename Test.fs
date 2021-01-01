6663c726de39b2adb3496728667c6fcb5d6b1601
Mikael Öhman <mikaeluman@gmail.com>
2021-01-01 02:16:35 +0100

    Add more tests

--- Test.fs
+++ Test.fs
@@ -10,3 +10,27 @@ module ``Score Calculation Tests``
         let r = Number(DiceSide.Four, DiceSideInstances.FourDie)
         let s = calculateSingleScore r
         s |> should equal 16
+
+    [<Fact>]
+    let ``When scoring two pairs of 2 and 6 the score is 16`` () =
+        let r = TwoPairs(DiceSide.Two, DiceSide.Six)
+        let s = calculateSingleScore r
+        s |> should equal 16
+
+    [<Fact>]
+    let ``When scoring a triple of threes the score is 9`` () =
+        let r = Multiple(DiceSide.Three, MultipleInstance.Triple)
+        let s = calculateSingleScore r
+        s |> should equal 9
+
+    [<Fact>]
+    let ``When scoring a pair of threes the score is 6`` () =
+        let r = Multiple(DiceSide.Three, MultipleInstance.Pair)
+        let s = calculateSingleScore r
+        s |> should equal 6
+
+    [<Fact>]
+    let ``When scoring a full house sixes full of ones the score is 20`` () =
+        let r = FullHouse(DiceSide.Six, DiceSide.One)
+        let s = calculateSingleScore r
+        s |> should equal 20
